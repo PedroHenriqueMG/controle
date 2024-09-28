e29ef83c17d65643dfc3b0f6870d6fa77648f166
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-09-27 21:33:26 -0300

    init project

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { bootstrapApplication } from "@angular/platform-browser";
+import {
+  PreloadAllModules,
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+} from "@angular/router";
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from "@ionic/angular/standalone";
+import { defineCustomElements } from "@ionic/pwa-elements/loader";
+
+import { AppComponent } from "./app/app.component";
+import { routes } from "./app/app.routes";
+
+defineCustomElements(window);
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+  ],
+});
