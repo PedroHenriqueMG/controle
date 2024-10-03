74cf58228280ef5af992a9689f5284be54f710c5
LucasTravessa <lucas.cos.travessa@gmail.com>
2024-10-03 19:27:19 -0300

    feat: floating tab navigator and new icons

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -7,7 +7,7 @@ import {
   IonLabel,
 } from "@ionic/angular/standalone";
 import { addIcons } from "ionicons";
-import { triangle, ellipse, square } from "ionicons/icons";
+import { triangle, ellipse, square, home, repeat } from "ionicons/icons";
 
 @Component({
   selector: "app-tabs",
@@ -20,6 +20,6 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ triangle, ellipse, square });
+    addIcons({ home, repeat });
   }
 }
