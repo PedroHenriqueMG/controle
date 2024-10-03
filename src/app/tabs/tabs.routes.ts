d4e753420882f1af0212e6040fcd1d681a058181
LucasTravessa <lucas.cos.travessa@gmail.com>
2024-10-03 19:15:20 -0300

    feat: home page

--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -3,24 +3,24 @@ import { TabsPage } from "./tabs.page";
 
 export const routes: Routes = [
   {
-    path: "tabs",
+    path: "",
     component: TabsPage,
     children: [
       {
-        path: "tab1",
+        path: "home",
         loadComponent: () =>
-          import("../tab1/tab1.page").then((m) => m.Tab1Page),
+          import("../home/home.page").then((m) => m.HomePage),
       },
       {
         path: "",
-        redirectTo: "/tabs/tab1",
+        redirectTo: "/home",
         pathMatch: "full",
       },
     ],
   },
   {
     path: "",
-    redirectTo: "/tabs/tab1",
+    redirectTo: "/home",
     pathMatch: "full",
   },
 ];
