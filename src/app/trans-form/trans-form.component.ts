d89de9b3844e16a7044c82b17a410810aa4ba8f9
LucasTravessa <lucas.cos.travessa@gmail.com>
2025-01-19 16:34:39 -0300

    feat: enhance transaction form with additional fields and modal for date selection

--- src/app/trans-form/trans-form.component.ts
+++ src/app/trans-form/trans-form.component.ts
@@ -1,20 +1,82 @@
-import { Component, OnInit } from "@angular/core";
-import { FormControl } from "@angular/forms";
-import { ReactiveFormsModule } from "@angular/forms";
+import { NgIf } from "@angular/common";
+import { Component, OnInit, signal } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
+import {
+  IonButton,
+  IonDatetime,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonModal,
+  IonSelect,
+  IonSelectOption,
+  IonTextarea,
+} from "@ionic/angular/standalone";
 
 @Component({
   selector: "app-trans-form",
   templateUrl: "./trans-form.component.html",
   styleUrls: ["./trans-form.component.scss"],
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [
+    NgIf,
+    IonInput,
+    IonTextarea,
+    IonDatetime,
+    IonModal,
+    IonSelect,
+    IonSelectOption,
+    IonButton,
+    IonLabel,
+    IonItem,
+    IonList,
+    ReactiveFormsModule,
+  ],
 })
 export class TransFormComponent implements OnInit {
-  constructor() {}
+  myForm!: FormGroup;
+  formStatus = signal<string>(""); // Signal to track form status
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit() {
-    console.log("Transform on");
+  ngOnInit(): void {
+    this.myForm = this.formBuilder.group({
+      type: ["", Validators.required],
+      amount: ["", Validators.required],
+      category: ["", Validators.required],
+      date: ["2023-01-01", Validators.required],
+      account: ["", Validators.required],
+      repeat: ["", Validators.required],
+      note: [""],
+    });
+
+    // Subscribe to form status changes using Signals
+    this.myForm.statusChanges.subscribe((status) => {
+      this.formStatus.set(status);
+    });
   }
-
-  name = new FormControl("");
+  onSubmit(): void {
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+      this.formStatus.set("Form submitted successfully!");
+    } else {
+      this.validateAllFormFields(this.myForm);
+      this.formStatus.set("Form contains errors.");
+    }
+  }
+  validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach((field) => {
+      const control = formGroup.get(field);
+      if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      } else {
+        control?.markAsTouched();
+      }
+    });
+  }
 }
