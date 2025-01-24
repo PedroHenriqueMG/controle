92b33f47852d02f2c3635fa6dbbeb69ef6c74b43
LucasTravessa <lucas.cos.travessa@gmail.com>
2025-01-24 13:12:31 -0300

    Revert "feat: defined drivers"
    This reverts commit 001614e797e596802f5ae295f27f271a6026ed67.

--- src/app/service/storage.service.ts
+++ src/app/service/storage.service.ts
@@ -14,8 +14,6 @@ export class StorageService {
 
   async init() {
     // If using, define drivers here: await this.storage.defineDriver(/*...*/);
-    await this.storage.defineDriver(indexedDB);
-    await this.storage.defineDriver(localStorage);
     const storage = await this.storage.create();
     this._storage = storage;
   }
